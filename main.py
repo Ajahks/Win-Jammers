d8eeec0f29319f5f92cd1451a237278a2945f1cb
Arren De Manuel <epicdragon81@gmail.com>
2021-09-16 08:12:29 +0000

    Basic Music bot and server

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import music
+import keep_alive
 
 cogs = [music]
 
@@ -9,5 +10,5 @@ client = commands.Bot(command_prefix='+', intents = discord.Intents.all())
 for i in range(len(cogs)):
   cogs[i].setup(client)
 
-
+keep_alive.keep_alive()
 client.run("ODg3OTI3NTU3MjcyNTMwOTg1.YULQ-g.YQtoMeKOdX2p-kOAy3cVRCLwacE")
